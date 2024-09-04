3e097c41be26d95d302d5680bed835a678af223d
Nikita Malinkin <nik.malina20@gmail.com>
2024-09-04 18:19:11 +0300

    Корректная работа программы для вывода напоминаний.
    fix: в классе NotificationRepository в SQL-запросе текущее время округлил до минут

--- src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
@@ -8,7 +8,7 @@ import pro.sky.telegrambot.model.Notification;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    @Query(value = "SELECT * FROM notification WHERE date = current_timestamp", nativeQuery = true)
+    @Query(value = "SELECT * FROM notification WHERE date = date_trunc('minute', now())", nativeQuery = true)
     Notification getCurrentNotification();
 }
 
